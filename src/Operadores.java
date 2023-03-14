4617d6df1cf684abab0f1a2fc008d690d1ecea67
mokshano11 <mateusggruber@gmail.com>
2023-03-14 13:49:05 -0400

    exercicios 5 op relacionais

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,14 +1,27 @@
 public class Operadores {
 
     public static void main(String[] args) throws Exception {
-
-    boolean variavel = true;
-
-    variavel = !variavel;
-
-    System.out.println(variavel);
-
     
+        String nomeUm = "Mateus";
+        String nomeDois = new String ("Mateus");
+
+        System.out.println(nomeUm.equals(nomeDois));  // utilizar .equals para relacionar OBJETOS
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numeroUm, e igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm, e diferente a numeroDois? " + simNao);
+
+        simNao = numero1 <= numero2;
+    
+        System.out.println("numeroUm, e menor a numeroDois? " + simNao);
     }
     
 }
