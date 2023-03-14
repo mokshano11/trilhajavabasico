498e3ab8747e3bdfe138dfb51b9bcfb24f8009d8
mokshano11 <mateusggruber@gmail.com>
2023-03-13 20:17:56 -0400

    exercicios 2

---
+++ src/Operadores.java
@@ -0,0 +1,25 @@
+public class Operadores {
+
+    public static void main(String[] args) throws Exception {
+
+        // operador =, atribuicao 
+   
+    String nome = "Mateus";
+    int idade = 27;
+    double peso = 70.3;
+    char sexo = 'M';
+    boolean doadorOrgao = false;
+    // Date dataNascimento = new Date();
+
+        // operadores + , -, /, *, %, aritmeticos 
+
+    double soma = 10.5 + 7.6;
+    int subtracao = 60 - 39;
+    int multiplicacao = 90 * 76;
+    int divisao = 98 / 8;
+    int modulo = 79 % 5;
+    double resultado = (10 * 7) + (24.6 / 4);
+    
+    }
+    
+}
